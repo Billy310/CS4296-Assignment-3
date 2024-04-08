6d0e7dd6c1ae6b2f0c16bd0dbb185c4e1532872f
Billy Gor <120034159+Billy310@users.noreply.github.com>
2024-04-09 01:02:55 +0800

    Final

--- Code/Decision Tree Classifier/main.py
+++ Code/Decision Tree Classifier/main.py
@@ -1,9 +1,17 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.classification import MultilayerPerceptronClassifier
+from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import VectorAssembler
 import time
+import argparse
 
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("train_csv_path", help="S3 path for train csv")
+parser.add_argument("test_csv_path", help="S3 path for test csv")
+parser.add_argument("predictions_path", help="S3 path for saving predictions")
+parser.add_argument("results_path", help="S3 path for saving results")
+args = parser.parse_args()
 # Start Spark Session
 spark = SparkSession.builder.appName("MNIST").getOrCreate()
 
@@ -11,32 +19,27 @@ spark = SparkSession.builder.appName("MNIST").getOrCreate()
 start_time = time.time()
 
 # Load training data
-train_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("s3://assignment3yuentatshingbilly/mnist_train.csv")
+train_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(args.train_csv_path)
 
 # Convert columns of pixels to a single vector
 assembler = VectorAssembler(inputCols=train_data.columns[1:], outputCol="features")
 train_data = assembler.transform(train_data)
 
-# Specify layers for the neural network:
-# input layer of size 784 (features), two intermediate layers
-# and output of size 10 (classes)
-layers = [784, 128, 64, 10]
-
 # Create the trainer and set its parameters
-trainer = MultilayerPerceptronClassifier(maxIter=100, layers=layers, blockSize=128, seed=1234)
+trainer = DecisionTreeClassifier(labelCol="label", featuresCol="features")
 
 # Train the model
 model = trainer.fit(train_data)
 
 # Load test data
-test_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("s3://assignment3yuentatshingbilly/mnist_test.csv")
+test_data = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(args.test_csv_path)
 test_data = assembler.transform(test_data)
 
 result = model.transform(test_data)
 predictionAndLabels = result.select("prediction", "label")
 
 # Write the predictions to a CSV file
-predictionAndLabels.write.format("csv").option("header", "true").save("s3://assignment3yuentatshingbilly/predictions.csv")
+predictionAndLabels.write.format("csv").option("header", "true").save(args.predictions_path)
 
 evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
 accuracy = evaluator.evaluate(result)
@@ -50,12 +53,11 @@ running_time = end_time - start_time
 train_result = model.transform(train_data)
 train_accuracy = evaluator.evaluate(train_result)
 
-
-
-
-
 # Write running time and test accuracy to a text file
 sc = spark.sparkContext
 # Write training accuracy to the same text file
 train_results = sc.parallelize([(running_time, train_accuracy, accuracy)])
-train_results.saveAsTextFile("s3://assignment3yuentatshingbilly/results.txt")
+train_results.saveAsTextFile(args.results_path)
+
+# Stop Spark Session
+spark.stop()
